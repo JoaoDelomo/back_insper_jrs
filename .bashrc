8bfc1cf4a1c721f3e1b1f40cf89073c339546a4e
JoaoDelomo <delominho@gmail.com>
2024-10-15 18:31:47 -0300

    changes

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export MONGO_USER="admin"
+export MONGO_PASSWORD="admin"
--- Projeto_Cursinho/models/database.py
+++ Projeto_Cursinho/models/database.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
+import os
+
 
 def get_db():
-    client = MongoClient('mongodb+srv://admin:admin@delomo.zxqnf.mongodb.net/')
-    db = client['JRs']
+    mongo_user = os.getenv('MONGO_USER')
+    mongo_password = os.getenv('MONGO_PASSWORD')
+    connection_string = f"mongodb+srv://{mongo_user}:{mongo_password}@delomo.zxqnf.mongodb.net/?authSource=admin&retryWrites=true&w=majority"
+    
+    client = MongoClient(connection_string)
+    db = client['Jrs']  
     return db
