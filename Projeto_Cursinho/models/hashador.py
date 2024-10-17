2fc82841332a89104277e6736e0bd630dd500862
guikaidei <guigokaidei@gmail.com>
2024-10-17 11:39:57 -0300

    modelo data

--- Projeto_Cursinho/models/hashador.py
+++ Projeto_Cursinho/models/hashador.py
@@ -59,6 +59,118 @@ aluno_maria = {
   ]
 }
 
+
+usuario1 = {
+    "nome": "João",
+    "matricula": "12345",
+    "senha": senha_joao,
+    "tipo": "aluno",
+
+    "materias": [
+        {
+            "nome": "Matemática",
+            "professor": "Pedro",
+            "notas": [
+                {"atividade": "Prova 1", "nota": 9.5},
+                {"atividade": "Prova 2", "nota": 8.0}
+            ],
+            "avisos": [
+                {"titulo": "Prova 3", "mensagem": "A prova 3 será no dia 10/12."}
+            ]
+        },
+        {
+            "nome": "História",
+            "professor": "Ana",
+            "notas": [
+                {"atividade": "Prova 1", "nota": 8.0},
+                {"atividade": "Prova 2", "nota": 7.5}
+            ],
+            "avisos": [
+                {"titulo": "Trabalho final", "mensagem": "O trabalho final deve ser entregue até o dia 15/12."}
+            ]
+        }
+    ],
+
+    "grade_horaria": [
+        {"Matematica": "8:00"},
+        {"História": "10:00"}
+    ]
+} 
+
+usuario2 = {
+    "nome": "Maria",
+    "matricula": "54321",
+    "senha": senha_maria,
+    "tipo": "aluno",
+
+    "materias": [
+        {
+            "nome": "Matemática",
+            "professor": "Pedro",
+            "notas": [
+                {"atividade": "Prova 1", "nota": 8.7},
+                {"atividade": "Prova 2", "nota": 8.0}
+            ],
+            "avisos": [
+                {"titulo": "Prova 3", "mensagem": "A prova 3 será no dia 10/12."}
+            ]
+        },
+        {
+            "nome": "História",
+            "professor": "Ana",
+            "notas": [
+                {"atividade": "Prova 1", "nota": 9.0},
+                {"atividade": "Prova 2", "nota": 9.5}
+            ],
+            "avisos": [
+                {"titulo": "Trabalho final", "mensagem": "O trabalho final deve ser entregue até o dia 15/12."}
+            ]
+        }
+    ],
+
+    "grade_horaria": [
+        {"Matematica": "8:00"},
+        {"História": "10:00"}
+    ]
+
+}
+
+usuario3 = {
+    "nome": "Pedro",
+    "matricula": "13345",
+    "senha": senha_joao,
+    "tipo": "professor",
+
+    "materias": ["matematica"],
+
+    "grade_horaria": [
+        {"Matematica": "8:00"}
+    ]
+}
+
+usuario4 = {
+    "nome": "Ana",
+    "matricula": "44321",
+    "senha": senha_maria,
+    "tipo": "professor",
+
+    "materias": ["história"],
+
+    "grade_horaria": [
+        {"História": "10:00"}
+    ]
+
+}
+
+usuario5= {
+    "nome": "Jorge",
+    "matricula": "1222",
+    "senha": senha_joao,
+    "tipo": "gestor",
+    "materias": [],
+    "grade_horaria": []
+}
+
 # Inserir os alunos no MongoDB
 db = get_db()
 db['alunos'].insert_many([aluno_joao, aluno_maria])
